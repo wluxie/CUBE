23d4628230a9c4df490eefde424582e92f99abd8
wluxie <49565505+wluxie@users.noreply.github.com>
2021-06-16 17:48:40 -0700

    Delete CUBE directory

--- CUBE/MenuAsset.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-public class MenuAsset extends Actor
-{
-    public void act() 
-    {
-        // Add your action code here.
-    }    
-}
